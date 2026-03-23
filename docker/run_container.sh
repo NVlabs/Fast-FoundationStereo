6171a1424fddbceb1ee3d5d7cf8c31a74480f8f1
kvnptl <kevinpatel4400@gmail.com>
2026-03-23 15:50:36 +0100

    Docker: X11/Qt libs and run script for host GUI

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,4 @@
 docker rm -f ffs
 DIR=$(pwd)/../
-docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name ffs --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:/workspace --ipc=host -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp -v /home:/home -v /mnt:/mnt ffs bash
+mkdir -p /tmp/runtime
+docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name ffs --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:/workspace --ipc=host -e DISPLAY=${DISPLAY} -e QT_X11_NO_MITSHM=1 -e QT_DEBUG_PLUGINS=0 -e XDG_RUNTIME_DIR=/tmp/runtime -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp -v /home:/home -v /mnt:/mnt ffs bash
