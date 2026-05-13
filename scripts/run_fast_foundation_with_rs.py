ec6e9010af18156f783c294c236f0ebf199fb845
UD31415 <uri.dubin@realsenseai.com>
2026-05-13 15:37:23 +0300

    before updates

--- scripts/run_fast_foundation_with_rs.py
+++ scripts/run_fast_foundation_with_rs.py
@@ -750,8 +750,8 @@ if __name__ == '__main__':
     # test_point_cloud_rt() # ok
     #test_point_cloud_rs_versus_foundation()
     #test_video_stream_rs_versus_foundation_x2() # ok
-    #test_video_stream_rs_fs_merge()
-    test_faro_rs_fs_error()
+    test_video_stream_rs_fs_merge()
+    #test_faro_rs_fs_error()
 
 
 
